676a9d03653a21b8125a20c03a73d69a5bd09f60
joel verano <veranojoel16@gmail.com>
2024-10-15 08:58:26 +0800

    initial commit

---
+++ home/inventory/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inventory, name='inventory'),
+    path('invoice/', views.invoice, name='invoice'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('history/', views.history, name='history'),
+]
