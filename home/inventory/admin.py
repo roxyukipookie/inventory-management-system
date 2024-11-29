47776937a481cf9af877aa218217e4830a9a26e8
joel verano <veranojoel16@gmail.com>
2024-11-29 12:27:06 +0800

    unfinished

--- home/inventory/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Category, AddProduct
-
-admin.site.register(Category)
-admin.site.register(AddProduct)
--- home/inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inventory, name='inventory'),
-    path('invoice/', views.invoice, name='invoice'),
-    path('add_product/', views.add_product, name='add_product'),
-    path('history/', views.history, name='history'),
-]
